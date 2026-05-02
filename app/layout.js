da91323b215d4b50a9849c163b30ddfacf7644cf
Mio0000 <2312110040k@kindai.ac.jp>
2026-05-02 15:43:36 +1000

    Fix layout syntax error

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+  import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 export const metadata = {
   title: "Cafe",
@@ -12,6 +14,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />         
       </body>
     </html>
   );
